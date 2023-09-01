1182679e71b82be010ee0b000f7e0db9f5601305
suoken <kensuong@gmail.com>
2023-08-31 23:09:00 -0700

    feat: created home page template

--- mlb-scouting-report/mlb_scouting_report/mlb_scouting_report/urls.py
+++ mlb-scouting-report/mlb_scouting_report/mlb_scouting_report/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import homePage
+
 urlpatterns = [
+    path('', homePage, name='homePage'),
     path('admin/', admin.site.urls),
 ]
