d5b5288f04a905ea57e13f18b9caaf06be838179
suoken <kensuong@gmail.com>
2023-09-07 17:31:31 -0700

    feat: created db models

--- mlb-scouting-report/mlb_scouting_report/player/models.py
+++ mlb-scouting-report/mlb_scouting_report/player/models.py
@@ -1,11 +1,166 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+
+class ThrowingArm(models.TextChoices):
+    RIGHT = "R", "Right"
+    LEFT = "L", "Left"
+
+class BattingPosition(models.TextChoices):
+    RIGHT = "R", "Right"
+    LEFT = "L", "Left"
+    SWITCH = "Switch"
+
+class PitchingPositions(models.TextChoices):
+    STARTING_PITCHER = "SP", "Starting Pitcher"
+    RELIEF_PITCHER = "RP", "Relief Pitcher"
+    CLOSING_PITCHER = "CL", "Closing Pitcher"
+
+class FieldPosition(models.TextChoices):
+    PITCHER = "P", "Pitcher"
+    CATCHER = "C", "Catcher"
+    FIRST_BASEMAN = "1B", "First Baseman"
+    SECOND_BASEMAN = "2B", "Second Baseman"
+    THIRD_BASEMAN = "3B", "Third Baseman"
+    SHORTSTOP = "SS", "Shortstop"
+    LEFT_FIELD = "LF", "Left Field"
+    CENTER_FIELD = "CF", "Center Field"
+    RIGHT_FIELD = "RF", "Right Field"
+    DESIGNATED_HITTER = "DH", "Designated Hitter"
+
+class ToolGrades(models.IntegerChoices):
+    """
+    Tool Grades increments are based on the 20-80 Scouting Scale
+    """
+    THIRTY = 30
+    THIRTY_FIVE = 35
+    FOURTY = 40
+    FOURTY_FIVE = 45
+    FIFTY = 50
+    FIFTY_FIVE = 55
+    SIXTY = 60
+    SIXTY_FIVE = 65
+    SEVENTY = 70
+    SEVENTY_FIVE = 75
+    EIGHTY = 80
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ("name",)
     
     def __str__(self):
         return self.name
+    
+class Team(models.Model):
+    code = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Hitter(models.Model):
+    """
+    Represents a baseball hitter with various skill ratings and assessments
+    """
+    category = models.ForeignKey(Category, related_name="hitters", on_delete=models.CASCADE)
+    player_name = models.CharField(max_length=255)
+
+    report_date = models.DateField(auto_now_add=True)
+    declarative_statement = models.TextField(blank=True, null=True)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name="hitters")
+    field_position = models.CharField(max_length=10, choices=FieldPosition.choices)
+    batting_position = models.CharField(max_length=10, choices=BattingPosition.choices)
+    throwing_arm = models.CharField(max_length=10, choices=ThrowingArm.choices)
+
+    hit = models.IntegerField(verbose_name="Hit rating", help_text="Hit rating between 30 to 80", choices=ToolGrades.choices)
+    hit_future_value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    hit_comments = models.TextField(max_length=255, blank=True, null=True)
+
+    power = models.IntegerField(verbose_name="Power rating", help_text="Power rating between 30 to 80", choices=ToolGrades.choices)
+    power_future_value = models.IntegerField(choices=ToolGrades.choices)
+    power_comments = models.TextField(max_length=255, blank=True, null=True)
+
+    fielding = models.IntegerField(choices=ToolGrades.choices)
+    fielding_future_value = models.IntegerField(choices=ToolGrades.choices)
+    fielding_comments = models.TextField(max_length=255, blank=True, null=True)
+    
+    throwing = models.IntegerField(choices=ToolGrades.choices)
+    throwing_future_value = models.IntegerField(choices=ToolGrades.choices)
+    throwing_comments = models.TextField(max_length=255, blank=True, null=True)
+    
+    run = models.IntegerField(choices=ToolGrades.choices)
+    run_future_value = models.IntegerField(choices=ToolGrades.choices)
+    run_comments = models.TextField(max_length=255, blank=True, null=True)
+
+    overall_grade = models.IntegerField(choices=ToolGrades.choices)
+    future_grade = models.IntegerField(choices=ToolGrades.choices)
+
+    class Meta:
+        ordering = ("-report_date",)
+
+    def __str__(self):
+        return self.player_name
+
+class Pitcher(models.Model):
+    """
+    Represents a baseball pitcher with various skill ratings and assessments
+    """
+    category = models.ForeignKey(Category, related_name="pitchers", on_delete=models.CASCADE)
+    player_name = models.CharField(max_length=255)
+
+    report_date = models.DateField(auto_now_add=True)
+    declarative_statement = models.TextField(max_length=1024, blank=True, null=True)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name="pitchers")
+    position = models.CharField(max_length=10, choices=PitchingPositions.choices)
+    throwing_arm = models.CharField(max_length=20, choices=ThrowingArm.choices)
+
+    overall_grade = models.IntegerField(choices=ToolGrades.choices)
+    future_grade = models.IntegerField(choices=ToolGrades.choices)
+
+    class Meta:
+        ordering = ("-report_date",)
+
+    def __str__(self):
+        return self.player_name
+
+class Pitch(models.Model):
+    """
+    Represents the pitch that the pitcher throws and the evaluation of the pitch
+    """
+    class PitchType(models.TextChoices):
+        FOUR_SEAM_FASTBALL = "Four-seam Fastball"
+        TWO_SEAM_FASTBALL = "Two-seam Fastball"
+        CUTTER = "Cutter"
+        SPLITTER = "Splitter"
+        SLIDER = "Slider"
+        CURVEBALL = "Curveball"
+        FORKBALL = "Forkball"
+        SLURVE = "Slurve"
+        SCREWBALL = "Screwball"
+        CHANGEUP = "Changeup"
+        PALMBALL = "Palmball"
+        CIRCLE_CHANGEUP = "Circle Changeup"
+        KNUCKLEBALL = "Kunckleball"
+        EEPHUS = "Eephus"
+        KNUCKLE_CURVE = "Knuckle Curve"
+        SINKER = "Sinker"
+        SWEEPER = "Sweeper"
+
+    pitcher = models.ForeignKey(Pitcher, related_name="pitches", on_delete=models.CASCADE)
+    pitch_type = models.CharField(max_length=20, choices=PitchType.choices)
+    velocity_low = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+    velocity_high = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+    grade = models.IntegerField(choices=ToolGrades.choices)
+    pitch_future_value = models.IntegerField(choices=ToolGrades.choices)
+    comments = models.TextField(max_length=1024)
+
+    def clean(self):
+        if self.velocity_low > self.velocity_high:
+            raise ValidationError("velocity_low cannot be greater than velocity_high") # handle this gracefully
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
