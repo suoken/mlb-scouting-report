68d418f9f987739bb8d2be3d811dd30333ea6e9b
suoken <kensuong@gmail.com>
2023-09-10 09:16:19 -0700

    feat: RSS feed partially added

--- mlb-scouting-report/mlb_scouting_report/core/views.py
+++ mlb-scouting-report/mlb_scouting_report/core/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+import feedparser
+import datetime
 
 from player.models import Hitter, Pitcher, Player
 
 def homePage(request):
     hitters = Hitter.objects.all()[0:4]
     pitchers = Pitcher.objects.all()[0:4]
-    return render(request, 'core/homepage.html', {'hitters': hitters, 'pitchers': pitchers})
+
+
+    feed_url = 'https://www.mlb.com/feeds/news/rss.xml'
+    feed = feedparser.parse(feed_url)
+
+    feed_entries = feed.entries[:4]
+
+    for entry in feed_entries:
+        epoch_timestamp = int(entry['mlb_display-date-epoch'])
+        date_obj = datetime.datetime.fromtimestamp(epoch_timestamp)
+        entry['formatted_date'] = date_obj.strftime('%b %d %Y')
+
+    return render(request, 'core/homepage.html', {'hitters': hitters, 'pitchers': pitchers, 'feed': feed_entries})
 
 def pitchingReport(request):
     return render(request, 'core/pitchingreport.html')
+
