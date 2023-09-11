cecfa5140ead9dbb7ff49018327244c0f32921f9
suoken <kensuong@gmail.com>
2023-09-10 17:45:02 -0700

    fix: improved UI for pitching report

--- mlb-scouting-report/mlb_scouting_report/player/views.py
+++ mlb-scouting-report/mlb_scouting_report/player/views.py
@@ -19,47 +19,37 @@ def playerHittingReport(request, slug):
             ('Thr', stat.throwing_arm)
         ]
     ]
-
-    tool_fields = [
-        (label, value)
-        for stat in hitter_stats
-        for label, value in [
-            ('Hit', stat.hit),
-            ('Hit FV', stat.hit_future_value),
-            ('Hit Comment', stat.hit_comments),
-            ('Power', stat.power),
-            ('Power FV', stat.power_future_value),
-            ('Power Comment', stat.power_comments),
-            ('Field', stat.fielding),
-            ('Field FV', stat.fielding_future_value),
-            ('Field Comment', stat.fielding_comments),
-            ('Throw', stat.throwing),
-            ('Throw FV', stat.throwing_future_value),
-            ('Throw Comment', stat.throwing_comments),
-            ('Run', stat.run),
-            ('Run FV', stat.run_future_value),
-            ('Run Comment', stat.run_comments),
-        ]
-    ]
-
-    print(tool_fields)
-
-    return render(request, 'player/player-hitting-report.html', {'player': player, 'stat_fields': stat_fields, 'latest_hitter_record': latest_hitter_record, 'tool_fields': tool_fields})
+    
+    return render(request, 'player/player-hitting-report.html', {'player': player, 'stat_fields': stat_fields, 'latest_hitter_record': latest_hitter_record})
 
 def playerPitchingReport(request, slug):
     player = get_object_or_404(Player, slug=slug)
-    return render(request, 'player/player-pitching-report.html', {'player': player})
+    latest_pitcher_record = player.pitchers.last()
+    pitcher_stats = player.pitchers.all()
+    pitches = [pitch for pitch in latest_pitcher_record.pitches.all()]
+    
+    stat_fields = [
+        (label, value)
+        for stat in pitcher_stats
+        for label, value in [
+            ('Date', stat.report_date),
+            ('Team', player.team),
+            ('Pos', stat.position),
+            ('Thr', stat.throwing_arm)
+        ]
+    ]
 
-
+    return render(request, 'player/player-pitching-report.html', {
+                'player': player, 
+                'stat_fields': stat_fields, 
+                'latest_pitcher_record': latest_pitcher_record, 
+                'pitches': pitches
+                })
 
 def createHittingReport(request):
     tools = ["Hit", "Power", "Fielding", "Throwing", "Run"]
     if request.method == 'POST':
         form = HittingReportForm(request.POST)
-
-        print(request.POST)
-        print(form)
-
         if form.is_valid():
             form.save()
             return redirect('/')
